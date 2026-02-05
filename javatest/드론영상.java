2dd1c135e369b43bf4f224103e25ba8a35d2e32d
masterJKR <nilzero25@gamil.com>
2026-02-05 09:05:01 +0900

    2월 4일 보강

---
+++ javatest/드론영상.java
@@ -0,0 +1,12 @@
+package javatest;
+
+import java.time.LocalDate;
+
+public class 드론영상 {
+    String 제목;
+    LocalDate 날짜;
+    public 드론영상(String 제목, LocalDate 날짜){
+        this.제목 = 제목;
+        this.날짜 = 날짜;
+    }
+}
